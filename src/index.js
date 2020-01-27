c1a417c083af23b00a8498903b0b58b904156357
J B <justinian.boghici@gmail.com>
2020-01-27 21:50:18 +0000

    ES6 - Use Spread Operator on object

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-// ES6 - Spread Operator:
-const first = [1, 2, 3];
-const second = [4, 5, 6];
+// ES6 - Spread Operator on object:
+const first = { name: 'Justin' };
+const second = { job: 'Front End Developer' };
 
-const combined = first.concat(second);
+const combined = { ...first, ...second, location: 'UK' };
+const clone = { ...first };
 
-// with spread operator becomes:
-const combined = [...first, ...second];
+// // ES6 - Spread Operator on array:
+// const first = [1, 2, 3];
+// const second = [4, 5, 6];
 
-// advantage being easier to add another element later on:
-const combined = [...first, 'a', ...second, 'b'];
+// const combined = first.concat(second);
 
-// also easy to clone an array:
-const clone = [...first];
+// // with spread operator becomes:
+// const combined = [...first, ...second];
 
-// ES6 - Object Destructuring:
+// // advantage being easier to add another element later on:
+// const combined = [...first, 'a', ...second, 'b'];
+
+// // also easy to clone an array:
+// const clone = [...first];
+
+// // ES6 - Object Destructuring:
 //
 // const address = {
 //   street: '',
@@ -25,15 +32,15 @@ const clone = [...first];
 // const city = address.city;
 // const country = address.country;
 //
-// with destructuring becomes:
+// // with destructuring becomes:
 //
 // const { street, city, country } = address;
 //
-// & assign different name to constant:
+// // & assign different name to constant:
 //
 // const { street: st } = address;
 
-// ES6 - Use Array.Map() to render list:
+// // ES6 - Use Array.Map() to render list:
 //
 // const colors = ['red', 'green', 'blue'];
 // const items = colors.map(color => `<li>${color}</li>`);
