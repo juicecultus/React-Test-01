1043bda31d3a78405c79a5e22cc80a83b8303bf1
J B <justinian.boghici@gmail.com>
2020-01-27 21:40:49 +0000

    ES6 - Use Spread Operator on array

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+// ES6 - Spread Operator:
+const first = [1, 2, 3];
+const second = [4, 5, 6];
+
+const combined = first.concat(second);
+
+// with spread operator becomes:
+const combined = [...first, ...second];
+
+// advantage being easier to add another element later on:
+const combined = [...first, 'a', ...second, 'b'];
+
+// also easy to clone an array:
+const clone = [...first];
+
 // ES6 - Object Destructuring:
 //
-const address = {
-  street: '',
-  city: '',
-  country: ''
-};
+// const address = {
+//   street: '',
+//   city: '',
+//   country: ''
+// };
 //
 // const street = address.street;
 // const city = address.city;
@@ -12,11 +27,11 @@ const address = {
 //
 // with destructuring becomes:
 //
-const { street, city, country } = address;
+// const { street, city, country } = address;
 //
 // & assign different name to constant:
 //
-const { street: st } = address;
+// const { street: st } = address;
 
 // ES6 - Use Array.Map() to render list:
 //
