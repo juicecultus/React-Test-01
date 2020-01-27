355a0181331f2a0e4606cf187e7fd5541497b73e
J B <justinian.boghici@gmail.com>
2020-01-27 21:32:09 +0000

    ES6 - Use Object Destructuring

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
-const colors = ['red', 'green', 'blue'];
-const items = colors.map(color => `<li>${color}</li>`);
+// ES6 - Object Destructuring:
+//
+const address = {
+  street: '',
+  city: '',
+  country: ''
+};
+//
+// const street = address.street;
+// const city = address.city;
+// const country = address.country;
+//
+// with destructuring becomes:
+//
+const { street, city, country } = address;
+//
+// & assign different name to constant:
+//
+const { street: st } = address;
+
+// ES6 - Use Array.Map() to render list:
+//
+// const colors = ['red', 'green', 'blue'];
+// const items = colors.map(color => `<li>${color}</li>`);
